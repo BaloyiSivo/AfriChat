2121d75bb21ae04389500d0745cd89eb9d3baa73
Baloyi Sivo <sivobaloyi@gmail.com>
2026-09-15 01:19:25 +0200

    Allow live frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,9 +19,12 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500",
-    ],
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://africhat.onrender.com",
+],
+
+
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
